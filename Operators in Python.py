59ac926892ea0228d377a3f0a5393b197dbaae0a
Rohith Neelagiriyappa <61341128+nrohith55@users.noreply.github.com>
2021-08-12 11:48:50 -0700

    Add files via upload

---
+++ Operators in Python.py
@@ -0,0 +1,87 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=2
+>>> y=3
+>>> x+y
+5
+>>> x-y
+-1
+>>> x*y
+6
+>>> x/y
+0.6666666666666666
+>>> x=8
+>>> x
+8
+>>> x=x+2
+>>> x
+10
+>>> x+=2
+>>> 
+>>> x
+12
+>>> x-=2
+>>> x
+10
+>>> x*=3
+>>> x
+30
+>>> a,b=8,2
+>>> a
+8
+>>> b
+2
+>>> n=7
+>>> n
+7
+>>> -n
+-7
+>>> =-n
+SyntaxError: invalid syntax
+>>> n=-n
+>>> n
+-7
+>>> a<b
+False
+>>> a>b
+True
+>>> a==b
+False
+>>> a=2
+>>> a==b
+True
+>>> a<=b
+True
+>>> a>=b
+True
+>>> a=8
+>>> b=9
+>>> a<=b
+True
+>>> a>=b
+False
+>>> a<=b
+True
+>>> a!b
+SyntaxError: invalid syntax
+>>> a!=b
+True
+>>> a==b
+False
+>>> a=5
+>>> b=4
+>>> a<8 and b<5
+True
+>>> a<8 and b<2
+False
+>>> a<8 or b<5
+True
+>>> x=True
+>>> x
+True
+>>> not x
+False
+>>> x=not x
+>>> x
+False
+>>> 
