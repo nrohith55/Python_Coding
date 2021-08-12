4d3082da1688fa71961840c26ea95e6a92bec976
Rohith Neelagiriyappa <61341128+nrohith55@users.noreply.github.com>
2021-08-12 12:06:28 -0700

    Add files via upload

---
+++ Number System Conversion.py
@@ -0,0 +1,21 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> bin(25)
+'0b11001'
+>>> 0b0101
+5
+>>> oct(25)
+'0o31'
+>>> hex(25)
+'0x19'
+>>> hex(10)
+'0xa'
+>>> 0xf
+15
+>>> bin(31)
+'0b11111'
+>>> bin(51)
+'0b110011'
+>>> 0b110011010
+410
+>>> 
