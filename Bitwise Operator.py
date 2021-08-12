c132ad589113252a4c53ebfe0d8679a54b8a4275
Rohith Neelagiriyappa <61341128+nrohith55@users.noreply.github.com>
2021-08-12 12:48:06 -0700

    Add files via upload

---
+++ Bitwise Operator.py
@@ -0,0 +1,21 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ~12
+-13
+>>> 12&13
+12
+>>> 12|13
+13
+>>> 25&30
+24
+>>> 12^13
+1
+>>> 12^1
+13
+>>> 25^30
+7
+>>> 10<<2
+40
+>>> 10>>2
+2
+>>> 
