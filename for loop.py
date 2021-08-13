1aeb172ce3a36681b2272b7fec5415b32b56c115
Rohith Neelagiriyappa <61341128+nrohith55@users.noreply.github.com>
2021-08-13 11:06:50 -0700

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,29 @@
+'''x=['Rohith',1,2,9.9]
+
+for i in x:
+    print(i)'''
+
+'''x='Rohith'
+for i in x:
+    print(i)'''
+
+'''for i in [1,2,3,4,5,'Paul']:
+    print(i)'''
+
+'''for i in range(10):
+    print(i)'''
+'''
+for i in range(10,20,2):
+    print(i)
+    '''
+'''
+for i in range(20,10,-1):
+    print(i)
+    '''
+
+for i in range(20):
+    if i%5!=0:
+        print(i)
+
+
+
---
+++ while loop.py
@@ -0,0 +1,24 @@
+'''i=1  ## Increment
+
+while i<=5:
+    print("Python")
+    i=i+1'''
+
+'''i=5 ## Decrement
+
+while i>=1:
+    print("Python", i)
+    i=i-1'''
+
+# Nested while loop
+
+i=1
+
+while i<=5:
+    print("Python",end="")
+    j=1
+    while j<=4:
+        print("Rocks",end="")
+        j=j+1
+    i=i+1
+    print()
