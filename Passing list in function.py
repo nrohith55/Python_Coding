845988266c9abf760f9c7b06bf2f84a237e9093e
Rohith Neelagiriyappa <61341128+nrohith55@users.noreply.github.com>
2021-08-15 13:31:48 -0700

    Add files via upload

---
+++ Passing list in function.py
@@ -0,0 +1,19 @@
+
+def count(lst):
+    even=0
+    odd=0
+
+    for i in lst:
+        if i%2==0:
+            even+=1
+        else:
+            odd+=1
+    return even,odd
+
+lst=[1,2,3,4,5,6,7,8,9,10]
+
+even,odd=count(lst)
+print(even)
+print(odd)
+
+print("Even : {} and Odd :{} ".format(even,odd))
